ea4480257b58fe4db3f29a8896714920efd13a73
liujiang <792041647@qq.com>
2019-07-19 21:29:38 +0800

    修改代码

--- no2Test/SingleTonTest.java
+++ no2Test/SingleTonTest.java
@@ -1,12 +1,15 @@
 public class SingleTonTest{
 	
 	public static void main(String[] args){
+		
 		SingleTon1 singleTon1 = SingleTon1.getInstance();
-
 		SingleTon1 singleTon2 = SingleTon1.getInstance();
-
-		System.out.println(singleTon1 == singleTon2);
-				
+		
+		if(singleTon1 == singleTon2){
+			System.out.println(true);
+		} else {
+			System.out.println(false);
+		}
 	}
 }
 
@@ -17,10 +20,12 @@ class SingleTon1{
 	private static SingleTon1 singleTon1 = null;
 
 	public static SingleTon1 getInstance(){
+		
 		if(singleTon1 == null){
 			singleTon1 = new SingleTon1();
-		}
+		} 
 		
 		return singleTon1;
+
 	}
 }
