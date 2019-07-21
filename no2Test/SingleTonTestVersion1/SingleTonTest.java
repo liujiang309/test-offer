2f9d92364fbf4908b033a834607c1fc93400812c
liujiang <792041647@qq.com>
2019-07-21 13:42:37 +0800

    测试成功

--- no2Test/SingleTonTestVersion1/SingleTonTest.java
+++ no2Test/SingleTonTestVersion1/SingleTonTest.java
@@ -1,30 +1,28 @@
-public class SingleTonTest{
-
-	public static void main(String[] args){
-		
-		SingleTon singleTonTest = SingleTon.getInstance();
-		SingleTon singleTonTest2 = SingleTon.getInstance();
-
-		if(singleTonTest == singleTonTest2){
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}		
-	}
-}
-
 class SingleTon{
 	
 	private SingleTon(){}
-
+	
 	private static SingleTon singleTon = null;
-
+	
 	public static SingleTon getInstance(){
-
 		if(singleTon == null){
 			singleTon = new SingleTon();
 		}
-		
 		return singleTon;
+	} 
+}
+
+public class SingleTonTest{
+	
+	public static void main(String[] args){
+		SingleTon singleTon1 = SingleTon.getInstance();
+		SingleTon singleTon2 = SingleTon.getInstance();
+		if(singleTon1 != null && singleTon2 != null){
+			if(singleTon1 == singleTon2){
+				System.out.println(true);
+			} else {
+				System.out.println(false);
+			}	
+		}
 	}
 }
--- no2Test/SingleTonTestVersion2/SingleTonTest.java
+++ no2Test/SingleTonTestVersion2/SingleTonTest.java
@@ -1,17 +1,3 @@
-public class SingleTonTest{
-	
-	public static void main(String[] args){
-		SingleTon singleTon = SingleTon.getInstance();
-		SingleTon singleTon2 = SingleTon.getInstance();
-
-		if(singleTon == singleTon2){
-			System.out.println(true);
-		} else {
-			System.out.println(false);
-		}
-	}
-}
-
 class SingleTon{
 	
 	private SingleTon(){}
@@ -20,14 +6,27 @@ class SingleTon{
 
 	public static SingleTon getInstance(){
 		if(singleTon == null){
-			
 			synchronized(SingleTon.class){
 				if(singleTon == null){
 					singleTon = new SingleTon();
 				}
 			}
 		}
-
 		return singleTon;
+	}
+}
+
+public class SingleTonTest{
+	
+	public static void main(String[] args){
+		SingleTon singleTon1 = SingleTon.getInstance();
+		SingleTon singleTon2 = SingleTon.getInstance();
+		if(singleTon1 != null && singleTon2 != null){
+			if(singleTon1 == singleTon2){
+				System.out.println(true);
+			} else {
+				System.out.println(false);
+			}	
+		}	
 	}
 }
