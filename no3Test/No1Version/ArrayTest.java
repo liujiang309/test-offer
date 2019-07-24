9cfdde5912f289fce59ffb215660d5aa54597cf2
liujiang <792041647@qq.com>
2019-07-24 22:47:30 +0800

    测试成功

--- no3Test/No1Version/ArrayTest.java
+++ no3Test/No1Version/ArrayTest.java
@@ -1,26 +1,43 @@
 import java.util.*;
+class ListNode{
+	
+	int val;
+	ListNode next;
+	
+	public ListNode(int val){
+		this.val = val;
+	}
+}
+
 public class ArrayTest{
 	
 	public static void main(String[] args){
 		int[] numbers = {2,3,1,0,2,5,3};
-		ArrayList<Integer> arrayList = new ArrayList<Integer>();
-		getDoubleNumber(numbers,7,arrayList);
-		System.out.println(arrayList);
-	}	
+		ArrayList<Integer> doubleNumber = new ArrayList<Integer>();
+		getDoubleNumber(numbers,doubleNumber);
+		System.out.println(doubleNumber);
+	}
 	
-	public static boolean getDoubleNumber(int[] numbers,int length,List<Integer> doubleNumber){
+	public static boolean getDoubleNumber(int[] numbers,List<Integer> doubleNumber){
 		boolean flag = false;
-		Set<Integer> hashSet = new HashSet<Integer>();
-		
-		for(int i = 0;i < length;i++){
-			if(hashSet.contains(numbers[i])){
-				doubleNumber.add(numbers[i]);
-				flag = true;
-			} else {
-				hashSet.add(numbers[i]);
+		if(numbers == null){
+			return false;
+		} else if(numbers.length == 0){
+			return false;
+		} else {
+			Set<Integer> hashSet = new HashSet<Integer>();
+			
+			for(int i = 0;i< numbers.length;i++){
+				if(hashSet.contains(numbers[i])){
+					doubleNumber.add(numbers[i]);
+					flag = true;
+				} else {
+					hashSet.add(numbers[i]);
+				}
 			}
+			
 		}
-
+			
 		return flag;
 	}
 }
