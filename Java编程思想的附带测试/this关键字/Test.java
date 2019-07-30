7c30c91d089dfd15f192fd0525d0cbcd8401bf9e
liujiang <792041647@qq.com>
2019-07-30 19:03:39 +0800

    修改成功

---
+++ Java编程思想的附带测试/this关键字/Test.java
@@ -0,0 +1,18 @@
+class Test{
+	
+	public static void main(String[] args){
+		A a1 = new A();
+		A a2 = new A();
+		
+		a1.func();
+		a2.func();		
+	}
+}
+
+class A{
+	
+	int func(){
+	  	return 1;
+	}
+}
+
---
+++ Java编程思想的附带测试/this关键字/Test2.java
@@ -0,0 +1,14 @@
+class Test{
+	void fun(){
+		System.out.println("测试成功");
+	}
+}
+
+public class Test2{
+	
+	public static void main(String[] args){
+		Test a1 = new Test();
+		this.fun();
+	
+	}
+}
