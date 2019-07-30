4cefeb6544f9bf3a5a674edca37e3d3a148265b3
liujiang <792041647@qq.com>
2019-07-30 18:52:37 +0800

    修改成功

---
+++ Java编程思想的附带测试/Test3.java
@@ -0,0 +1,16 @@
+class Test{
+	
+	private int i;
+	
+//	public Test(int i){
+//		this.i = i;
+//	}
+}
+
+public class Test3{
+	
+	public static void main(String[] args){
+		Test t = new Test();
+		
+	}
+}
