ccd112259e301818ba8a3b80bc861207286e5f9a
liujiang <792041647@qq.com>
2019-07-30 11:28:26 +0800

    Java编程思想“

--- no3Test/No2Version/ArrayTest.java
+++ no3Test/No2Version/ArrayTest.java
@@ -2,39 +2,33 @@ import java.util.*;
 public class ArrayTest{
 	
 	public static void main(String[] args){
-		int[] numbers = {1,1,2,2,3,4,3,4,3};
+		int[] numbers = {2,3,1,0,2,5,3};
 		ArrayList<Integer> doubleNumber = new ArrayList<Integer>();
-		getDoubleNumber(numbers,9,doubleNumber);
-		System.out.println(doubleNumber);
+		getDoubleNumber(numbers,doubleNumber);
+		System.out.println(doubleNumber);	
 	}
-	
-	public static boolean getDoubleNumber(int[] numbers,int length,List<Integer> doubleNumber){
-		boolean flag = false;		
-		if(numbers == null || length <= 0){
-			return false;
-		} 
-		
-		for(int i = 0;i < length;i++){
-			if(numbers[i] < 0 || numbers[i] > length -1){
-				return false;
-			}
-		}
 
-		for(int i = 0;i< length;i++){
-			while(numbers[i] != i){
-				
-				if(numbers[i] == numbers[numbers[i]]){
-					doubleNumber.add(numbers[i]);
-					flag = true;
-				} else {
+	public static boolean getDoubleNumber(int[] numbers,List<Integer> doubleNumber){
+		if(numbers == null){
+			return false;
+		} else if(numbers.length == 0){
+			return false;
+		} else {
+			int i = 0;
+			while(i < numbers.length){
+				if(i == numbers[i]){
+					i++;
+				} else if( numbers[numbers[i]] != numbers[i]){
 					int temp = numbers[i];
-					numbers[i] = numbers[numbers[i]];
-					numbers[numbers[i]] = temp;
+					numbers[i] = numbers[temp];
+					numbers[temp] = temp;
+				} else if( numbers[numbers[i]] == numbers[i]){
+					doubleNumber.add(numbers[i]);
+					return true;
 				}
-				
 			}
-		}
 		
-		return flag;
+			return false;
+		}
 	}
 }
