8b7c5cb4e2450d411c7f05d9f87f7c0ae424d52e
liujiang <792041647@qq.com>
2019-07-30 18:44:07 +0800

    修改成功2

---
+++ Java编程思想的附带测试/Test.java
@@ -0,0 +1,32 @@
+class Test{
+	
+	public static void main(String[] args){
+		char c = 'A';
+		print(c);
+	}
+
+	public static void print(byte b){
+		System.out.println("byte:" + b);
+	}
+
+	public static void print(short s){
+		System.out.println("short:" + s);
+	}
+
+	public static void print(int i){
+		System.out.println("int:" + i);
+	}
+
+//	public static void print(char c){
+//		System.out.println("char:" + c);
+//	}
+
+//	public static void print(float f){
+//		System.out.println("float:" + f);
+//	}
+
+	public static void print(double d){
+		System.out.println("double:" + d);
+	}
+
+}
